3a972d7e7e5dc8d5d241afe55a42f9f0c3be1f98
jeong ji soo <pyheuisw@naver.com>
2023-08-24 21:51:59 +0900

    2일차 강의파일

---
+++ Day02/src/Grammar.java
@@ -0,0 +1,41 @@
+/**
+
+public class AA {
+	
+	public void 배고파() {
+		
+	}
+	
+	public void 오짐마려() {
+		
+	}
+	
+}
+
+*/
+
+
+
+public class Grammar { //Grammar
+
+	
+		public static void main(String[] args) {
+			
+			//문법은 Grammar라고 출력한다.
+			/*시링을 주세요.
+			주세요
+			배고파요
+			*/
+			
+			//아버지 가방에 들어가신다.;
+			
+			
+			
+			
+			
+			System.out.println("문법은 Grammar야.");
+			
+		}
+		
+		
+}
---
+++ Day02/src/Grammar2.java
@@ -0,0 +1,9 @@
+
+public class Grammar2 { //클래스
+	
+	public static void main(String[] args) {
+		
+		//메서드(함수) = 메서드 블록
+	}
+	
+} //클래스블록
